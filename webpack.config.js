cd4f8be017e286339053f28f35c7fdd0e8912525
Jacek Koziol <jacek.koziol@xfive.co>
2022-03-11 12:47:15 +0100

    Added ESlint

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,45 @@
 
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
-// module.exports = {
-//   entry: './src/index.ts',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.tsx?$/,
-//         use: 'ts-loader',
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: ['.tsx', '.ts', '.js'],
-//   },
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-// };
-
-
 module.exports = {
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/
-        },
-      ]
-    },
-    resolve: {
-      extensions: ['.ts', '.js', '.tsx']
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-      new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-      new HtmlWebpackPlugin({
-        title: "Webpack Output",
-        template: './src/index.html'
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: './src/styles' },
-        ]
-      })
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
-    devServer: {
-      port: 9000,
-      client: {
-        overlay: true,
-        logging: 'info'
-      }
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Output',
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: './src/styles'},
+      ],
+    }),
+  ],
+  devServer: {
+    port: 9000,
+    client: {
+      overlay: true,
+      logging: 'info',
     },
+  },
 };
